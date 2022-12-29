820dcd4cf601c02e0e154114b6e203a7b6b1c13a
Zijun Yu <zijun.yu.joey@gmail.com>
2022-12-29 11:57:18 +0100

    remove unnecessary sorted requirement

--- project.v
+++ project.v
@@ -136,73 +136,67 @@ Proof with auto using insert_all.
 Qed.
 
 Lemma insert_diff_root : forall t x y,
-  sorted t ->
   y <> x ->
   elem_of y (insert x t) = elem_of y t.
 Proof.
   intros.
-  induction H.
-  - simpl. apply Nat.eqb_neq in H0. rewrite H0.
+  induction t.
+  - simpl. apply Nat.eqb_neq in H. rewrite H.
     destruct (ltbP y x); reflexivity.
-  - simpl. destruct (eqbP x n).
-    + subst. apply Nat.eqb_neq in H0. rewrite H0.
-      destruct (y <? n) eqn:Hyn;
-      solve [simpl; rewrite H0; rewrite Hyn; reflexivity].
-    + destruct (x <? n);
-      solve [simpl; destruct (eqbP y n); try reflexivity;
-        destruct (y <? n); auto].
+  - simpl. destruct (eqbP x n); try reflexivity.
+    destruct (ltbP x n);
+    solve [simpl; destruct (eqbP y n); try reflexivity;
+      destruct (ltbP y n); auto].
 Qed.
 
 Lemma insert_correct : forall t x y,
-  sorted t ->
   elem_of y (insert x t) = orb (elem_of y t) (y =? x).
 Proof with auto.
-  intros. induction H.
+  intros. induction t.
   - simpl. destruct (eqbP y x)... destruct (ltbP y x)...
   - unfold insert. destruct (eqbP x n).
     + (* inserted [x] = root [n] *)
       simpl. destruct (eqbP y n); subst...
-      * (* searching [y] <> root [n] *)
-        destruct (ltbP y n);
-        try rewrite <- IHsorted1; try rewrite <- IHsorted2;
-        rewrite insert_diff_root...
+      (* searching [y] <> root [n] *)
+      destruct (ltbP y n);
+      try rewrite <- IHt1; try rewrite <- IHt2;
+      rewrite insert_diff_root...
     + destruct (ltbP x n); fold insert.
       * (* inserted [x] < root [n] *)
         simpl. destruct (eqbP y n); subst...
         destruct (ltbP y n).
         -- (* searching [y] < root [n] *)
-           apply IHsorted1.
+           apply IHt1.
         -- (* searching [y] > root [n] *)
            assert (Hneq: x <> y) by lia.
-           rewrite <- IHsorted2.
+           rewrite <- IHt2.
            rewrite insert_diff_root...
       * (* inserted [x] > root [n] *)
         simpl. destruct (eqbP y n); subst...
         destruct (ltbP y n).
         -- (* searching [y] < root [n] *)
            assert (Hneq: x <> y) by lia.
-           rewrite <- IHsorted1.
+           rewrite <- IHt1.
            rewrite insert_diff_root...
         -- (* searching [y] > root [n] *)
-           apply IHsorted2.
+           apply IHt2.
 Qed.
 
 Lemma insert_correctP : forall t x y,
-  sorted t ->
   elem_ofP y (insert x t) <-> (elem_ofP y t) \/ (y = x).
 Proof with auto.
   intros; split; intros.
-  - rewrite elem_of_prop in H0.
-    rewrite insert_correct in H0...
+  - rewrite elem_of_prop in H.
+    rewrite insert_correct in H...
     destruct (elem_of y t) eqn: Helem.
     rewrite <- elem_of_prop in Helem...
     destruct (y =? x) eqn:Heq.
     rewrite Nat.eqb_eq in Heq... discriminate.
   - rewrite elem_of_prop.
     rewrite insert_correct...
-    destruct H0...
-    rewrite elem_of_prop in H0; intuition.
-    rewrite <- Nat.eqb_eq in H0; intuition.
+    destruct H...
+    rewrite elem_of_prop in H; intuition.
+    rewrite <- Nat.eqb_eq in H; intuition.
 Qed.
 
 From Coq Require Import Lists.List.
@@ -219,14 +213,6 @@ Fixpoint list_to_bst (l : list nat) : tree :=
   | x :: xs => insert x (list_to_bst xs)
   end.
 
-Lemma to_bst_sorted : forall (l : list nat),
-  sorted (list_to_bst l).
-Proof.
-  induction l.
-  - simpl; constructor.
-  - simpl. now apply insert_sorted.
-Qed.
-
 Lemma to_bst_still_contains : forall (e : nat) (l : list nat),
   (In e l) <-> elem_ofP e (list_to_bst l).
 Proof.
@@ -236,16 +222,14 @@ Proof.
     + simpl in H. destruct H.
       * subst. simpl.
         rewrite insert_correctP. right. reflexivity.
-        apply to_bst_sorted.
       * simpl.
         rewrite insert_correctP. left. now apply IHl.
-        apply to_bst_sorted.
   - induction l.
     + inversion H.
     + simpl in H. destruct (eqbP e a).
       * subst. simpl. left. reflexivity.
       * simpl. right.
-        rewrite insert_correctP in H; try apply to_bst_sorted.
+        rewrite insert_correctP in H.
         destruct H; try contradiction.
         now apply IHl in H.
 Qed.
